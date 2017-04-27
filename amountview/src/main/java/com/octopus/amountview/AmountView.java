e38fe86eebc13369f50fd0f0a5d83afaa88f4471
Octopus <420369556@qq.com>
2017-04-27 15:58:21 +0800

    complete code

---
+++ amountview/src/main/java/com/octopus/amountview/AmountView.java
@@ -0,0 +1,188 @@
+package com.octopus.amountview;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+
+/**
+ *
+ */
+public class AmountView extends FrameLayout {
+    private Context mContext;
+
+    private View rootView;
+    private EditText amountEt;
+    private Button reduceBtn;
+    private Button increaseBtn;
+
+    private AttributeSet mAttrs;
+
+    //------------------------attrs-----------------------------
+    private float textSize; // default 13sp
+    private int minValue = 1; // must no less than 1,default is 1
+    private int maxValue = 99; // default is 99
+
+    private OnChangeListener mOnChangeListener;
+    //------------------------attrs-----------------------------
+
+    public AmountView(Context context) {
+        this(context, null);
+    }
+
+    public AmountView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public AmountView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        mContext = context;
+        this.mAttrs = attrs;
+        initView();
+    }
+
+    private void initView() {
+        rootView = LayoutInflater.from(mContext).inflate(R.layout.layout_amount_view, null);
+
+        amountEt = (EditText) rootView.findViewById(R.id.amount_et);
+        amountEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // after changed, check minValue and maxValue. change button enable. call listener.
+                if (TextUtils.isEmpty(s.toString())) {
+                    amountEt.setText(String.valueOf(minValue));
+                } else {
+                    int amount = Integer.parseInt(amountEt.getText().toString());
+
+                    if (amount < minValue) {
+                        amountEt.setText(String.valueOf(minValue));
+                    } else if (amount > maxValue) {
+                        amountEt.setText(String.valueOf(maxValue));
+                    } else {
+                        if (amount > minValue) {
+                            reduceBtn.setEnabled(true);
+                        } else {
+                            reduceBtn.setEnabled(false);
+                        }
+
+                        if (amount < maxValue) {
+                            increaseBtn.setEnabled(true);
+                        } else {
+                            increaseBtn.setEnabled(false);
+                        }
+
+                        if (null != mOnChangeListener) {
+                            mOnChangeListener.onChanged(amount);
+                        }
+                    }
+                }
+            }
+        });
+
+        reduceBtn = (Button) rootView.findViewById(R.id.reduce_btn);
+        reduceBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                amountEt.setText(String.valueOf(Integer.parseInt(amountEt.getText().toString()) - 1));
+            }
+        });
+        increaseBtn = (Button) rootView.findViewById(R.id.increase_btn);
+        increaseBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                amountEt.setText(String.valueOf(Integer.parseInt(amountEt.getText().toString()) + 1));
+            }
+        });
+
+        if (mAttrs != null) {
+            TypedArray typedArray = mContext.obtainStyledAttributes(mAttrs, R.styleable.AmountView);
+
+//        roundColor = typedArray.getColor(R.styleable.RoundProgressBar_roundColor, Color.GRAY);
+//
+//        progress = typedArray.getFloat(R.styleable.RoundProgressBar_progress, 0);
+
+            textSize = typedArray.getDimension(R.styleable.AmountView_textSize, sp2px(8));
+            amountEt.setTextSize(textSize);
+            reduceBtn.setTextSize(textSize);
+            increaseBtn.setTextSize(textSize);
+
+            minValue = typedArray.getInteger(R.styleable.AmountView_minValue, 1);
+            maxValue = typedArray.getInteger(R.styleable.AmountView_maxValue, 99);
+//
+//        isShowProgress = typedArray.getBoolean(R.styleable.RoundProgressBar_isShowProgress, true);
+//
+//        titleStr = typedArray.getString(R.styleable.RoundProgressBar_titleStr);
+
+            typedArray.recycle();
+        }
+
+        amountEt.setText(String.valueOf(minValue));
+
+        addView(rootView);
+    }
+
+    //----------------------------- get/set ----------------------------------------
+    public float getTextSize() {
+        return textSize;
+    }
+
+    public void setTextSize(float textSize) {
+        this.textSize = textSize;
+        amountEt.setTextSize(textSize);
+        reduceBtn.setTextSize(textSize);
+        increaseBtn.setTextSize(textSize);
+    }
+
+    public int getMinValue() {
+        return minValue;
+    }
+
+    public void setMinValue(int minValue) {
+        this.minValue = minValue;
+    }
+
+    public int getMaxValue() {
+        return maxValue;
+    }
+
+    public void setMaxValue(int maxValue) {
+        this.maxValue = maxValue;
+    }
+
+    public OnChangeListener getOnChangeListener() {
+        return mOnChangeListener;
+    }
+
+    public void setOnChangeListener(OnChangeListener mOnChangeListener) {
+        this.mOnChangeListener = mOnChangeListener;
+    }
+    //----------------------------- get/set ----------------------------------------
+
+    private int sp2px(float spValue) {
+        final float fontScale = Resources.getSystem().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    public interface OnChangeListener {
+        void onChanged(int value);
+    }
+}
--- app/src/main/java/com/octopus/roundprogressbar/MainActivity.java
+++ app/src/main/java/com/octopus/roundprogressbar/MainActivity.java
@@ -1,25 +1,18 @@
 package com.octopus.roundprogressbar;
 
-import android.animation.Animator;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.animation.LinearInterpolator;
+import android.widget.Toast;
 
-import com.octopus.round_progressbar.RoundProgressBar;
+import com.octopus.amountview.AmountView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.rpb_item1)
-    RoundProgressBar rpbItem1;
-    @BindView(R.id.rpb_item3)
-    RoundProgressBar rpbItem3;
-
-    private boolean cancelAnim = false;
+    @BindView(R.id.amountview_1)
+    AmountView amountview1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,57 +26,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        rpbItem1.clearAnimation();
     }
 
     private void initView() {
-        rpbItem1.setOnClickListener(new View.OnClickListener() {
+//        amountview1.setAmountTextSize(DisplayUtils.sp2px(13));
+        amountview1.setOnChangeListener(new AmountView.OnChangeListener() {
             @Override
-            public void onClick(View v) {
-                ToastUtils.showToast("点击跳过广告，进入主界面");
-                rpbItem1.clearAnimation();
-                toMainActivity();
+            public void onChanged(int value) {
+                ToastUtils.showToast("--->" + value);
             }
         });
-        rpbItem1.startAnimation(3000, new LinearInterpolator(), new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animation) {
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                if (!cancelAnim) {
-                    ToastUtils.showToast("广告结束，进入主界面");
-                    toMainActivity();
-                }
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-                cancelAnim = true;
-                ToastUtils.showToast("动画取消");
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-            }
-        });
-
-        rpbItem3.setTitleStr("item3");
-        rpbItem3.setRoundColor(Color.YELLOW);
-        rpbItem3.setRoundInsideColor(Color.LTGRAY);
-        rpbItem3.setRoundProgressColor(Color.BLUE);
-        rpbItem3.setRoundWidth(DisplayUtils.dip2px(4));
-        rpbItem3.setTextProgressSize(DisplayUtils.sp2px(16));
-        rpbItem3.setTextTitleSize(DisplayUtils.sp2px(12));
-        rpbItem3.setTextProgressColor(Color.BLACK);
-        rpbItem3.setTextTitleColor(Color.CYAN);
-//        rpbItem3.resetProgress(50);
-        rpbItem3.startAnimation(2000, null, null);
-    }
-
-    private void toMainActivity() {
-
     }
 
 }
